54e5e61875c5bfd849bca7fbb289a7d63d11bb4d
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-16 14:40:57 +0630

    Updated Allow One Function Call.

--- AllowOnce.js
+++ AllowOnce.js
@@ -8,14 +8,9 @@ var once = function(fn) {
     }
 };
 
-// // fn = (a,b,c) => (a + b + c), calls = [[1,2,3],[2,3,6]]
-fn = (a,b,c) => (a * b * c), calls = [[5,7,4],[2,3,6],[4,6,8]]
+// // fn = (a,b,c) => (a + b + c)
+fn = (a,b,c) => (a * b * c)
 
-// let onceFn = once(fn)
-// console.log(onceFn(calls));
-
-
-// let fn = (a,b,c) => (a * b * c)
 let onceFn = once(fn)
 console.log(onceFn(5,7,4));
 console.log(onceFn(5,7,9));
