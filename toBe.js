0ea9f424666e08ec617170f2030ca5f7730f63e9
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-16 14:39:39 +0630

    Updated To Be Or Not To Be.

--- toBe.js
+++ toBe.js
@@ -1,1 +1,18 @@
-
+var expect = function(val) {
+    return {
+        toBe : function(comp){
+            if(val === comp){
+                return true;
+            }else{
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe : function(comp){
+            if(val !== comp){
+                return true;
+            }else{
+                throw new Error("Equal");
+            }
+        }
+    }
+};
